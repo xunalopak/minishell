150aa5fc2d66f1acb9459a6f7039e16d50ce5d31
xuna <romainchampliau@gmail.com>
2022-03-22 20:21:49 +0100

    ta mère

---
+++ minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rchampli <rchampli@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 13:45:58 by rchampli          #+#    #+#             */
+/*   Updated: 2022/03/22 19:25:14 by rchampli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <errno.h>
+
+//global
+
+int						g_error;
+int						g_status;
+
+//env.c
+void	print_env(char **envp);
+
+#endif
