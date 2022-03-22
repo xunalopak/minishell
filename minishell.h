bdd56fc2a1c4386ac9604bb3c1eae68f1de833f5
xuna <romainchampliau@gmail.com>
2022-03-22 20:23:16 +0100

    ta mère

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: rchampli <rchampli@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 13:45:58 by rchampli          #+#    #+#             */
-/*   Updated: 2022/03/22 19:25:14 by rchampli         ###   ########.fr       */
+/*   Updated: 2022/03/22 20:23:12 by rchampli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,5 @@
 # include <sys/types.h>
 # include <errno.h>
 
-//global
-
-int						g_error;
-int						g_status;
-
-//env.c
-void	print_env(char **envp);
 
 #endif
