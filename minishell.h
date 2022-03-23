51cf12f04eda04400609c528bd59dccba092c289
Xuna <romainchampliau@gmail.com>
2022-03-23 13:50:23 +0100

    Ca change pas grand chose

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: rchampli <rchampli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 13:45:58 by rchampli          #+#    #+#             */
-/*   Updated: 2022/03/23 03:07:30 by rchampli         ###   ########.fr       */
+/*   Updated: 2022/03/23 13:36:16 by rchampli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,9 @@
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <errno.h>
+
+//signal
+void	sigint_handler(int sign_num, pid_t pid);
 
 //exit
 void		ft_exit(void);
