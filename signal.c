4346fee06062bc10ed1657fec13b781dea34f9f3
Xuna <romainchampliau@gmail.com>
2022-03-23 17:34:25 +0100

    Ca change pas grand chose

--- signal.c
+++ signal.c
@@ -6,36 +6,21 @@
 /*   By: rchampli <rchampli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 03:26:50 by rchampli          #+#    #+#             */
-/*   Updated: 2022/03/23 13:34:00 by rchampli         ###   ########.fr       */
+/*   Updated: 2022/03/23 17:34:02 by rchampli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 //signal(SIGINT, sigint_handler);
 
-void	process(int sign_num, pid_t pid)
+// peut etre recuper le pid sur un fork dans l'execution
+void	sigint_handler(int sign_num)
 {
-	if (!kill(pid, sign_num))
-	{
-		if (sign_num == SIGQUIT)
-			printf("Quit: 3\n");
-		else if (sign_num == SIGINT)
-			printf("\n");
-	}
-	else if (sign_num == SIGINT)
+	if (sign_num == SIGINT)
 		printf("\n");
+		// control+c
+	else if (sign_num == SIGQUIT)
+		printf("\b\b  \b\b");
+		/* control+\ */
 }
 
-// peut etre recuper le pid sur un fork dans l'execution
-void	sigint_handler(int sign_num, pid_t pid)
-{
-	if ((sign_num == SIGINT || sign_num == SIGQUIT) && pid != 0)
-		process(sign_num, pid);
-	else
-	{
-		if (sign_num == SIGINT)
-			printf("\n");
-		else if (sign_num == SIGQUIT)
-			printf("\b\b  \b\b");
-	}
-}
